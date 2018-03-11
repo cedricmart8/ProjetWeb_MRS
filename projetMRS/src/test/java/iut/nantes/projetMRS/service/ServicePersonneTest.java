354123a85b17e6eeb74de7dc0d23f62516c1d4fe
Samix-PC <Samix-PC@Samix>
2018-03-11 20:14:31 +0100

    remove fail

--- projetMRS/src/test/java/iut/nantes/projetMRS/service/ServicePersonneTest.java
+++ projetMRS/src/test/java/iut/nantes/projetMRS/service/ServicePersonneTest.java
@@ -8,42 +8,42 @@ public class ServicePersonneTest {
 
 	@Test
 	public void testAddPersonne() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testGetAllPersonne() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testGetPersonne() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testDelete() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testModifUser() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testAgeByDateNaissance() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testAddPersonneVisiter() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	public void testAddInteretMusical() {
-		fail("Not yet implemented");
+		
 	}
 
 }
