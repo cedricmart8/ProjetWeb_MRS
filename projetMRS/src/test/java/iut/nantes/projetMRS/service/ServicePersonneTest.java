23008e58affa503e415b3114c0682adb8c6fd1cb
Samix-PC <Samix-PC@Samix>
2018-03-11 20:16:04 +0100

    tst

--- projetMRS/src/test/java/iut/nantes/projetMRS/service/ServicePersonneTest.java
+++ projetMRS/src/test/java/iut/nantes/projetMRS/service/ServicePersonneTest.java
@@ -8,42 +8,42 @@ public class ServicePersonneTest {
 
 	@Test
 	public void testAddPersonne() {
-		
+		assertTrue( true );
 	}
 
 	@Test
 	public void testGetAllPersonne() {
-		
+		assertTrue( true );
 	}
 
 	@Test
 	public void testGetPersonne() {
-		
+		assertTrue( true );
 	}
 
 	@Test
 	public void testDelete() {
-		
+		assertTrue( true );
 	}
 
 	@Test
 	public void testModifUser() {
-		
+		assertTrue( true );
 	}
 
 	@Test
 	public void testAgeByDateNaissance() {
-		
+		assertTrue( true );
 	}
 
 	@Test
 	public void testAddPersonneVisiter() {
-		
+		assertTrue( true );
 	}
 
 	@Test
 	public void testAddInteretMusical() {
-		
+		assertTrue( true );
 	}
 
 }
