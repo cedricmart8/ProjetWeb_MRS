f7906de25b5d0927f1e670eab9b9da05cf7d73b7
Samix-PC <Samix-PC@Samix>
2018-03-11 20:17:11 +0100

    com

--- projetMRS/src/test/java/iut/nantes/projetMRS/service/ServicePersonneTest.java
+++ projetMRS/src/test/java/iut/nantes/projetMRS/service/ServicePersonneTest.java
@@ -5,45 +5,45 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ServicePersonneTest {
-
+/*
 	@Test
 	public void testAddPersonne() {
-		assertTrue( true );
+
 	}
 
 	@Test
 	public void testGetAllPersonne() {
-		assertTrue( true );
+
 	}
 
 	@Test
 	public void testGetPersonne() {
-		assertTrue( true );
+
 	}
 
 	@Test
 	public void testDelete() {
-		assertTrue( true );
+
 	}
 
 	@Test
 	public void testModifUser() {
-		assertTrue( true );
+
 	}
 
 	@Test
 	public void testAgeByDateNaissance() {
-		assertTrue( true );
+
 	}
 
 	@Test
 	public void testAddPersonneVisiter() {
-		assertTrue( true );
+
 	}
 
 	@Test
 	public void testAddInteretMusical() {
-		assertTrue( true );
-	}
 
+	}
+*/
 }
