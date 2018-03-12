84784887df4dc19dfb69a910b7d192ab5bef79be
samixchoumi <samixchoumi@gmail.com>
2018-03-12 12:11:07 +0100

    add test AddPersonne()

--- projetMRS/src/test/java/iut/nantes/projetMRS/service/ServicePersonneTest.java
+++ projetMRS/src/test/java/iut/nantes/projetMRS/service/ServicePersonneTest.java
@@ -2,21 +2,32 @@ package iut.nantes.projetMRS.service;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 import org.junit.Test;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
 import com.mongodb.MongoClient;
 
+import iut.nantes.projetMRS.entity.EntityPersonne;
+
 @SuppressWarnings("unused")
 public class ServicePersonneTest {
 
-	MongoClient client = new MongoClient("localhost", 8081); // connect to mongodb
-	Datastore datastore = new Morphia().createDatastore(client, "service"); // select service collection
+	ServicePersonne sp = new ServicePersonne();
 	
+	@SuppressWarnings("deprecation")
 	@Test
 	public void testAddPersonne() {
-		
+		EntityPersonne eP = new EntityPersonne(
+				"NomTest", "PrenomTest", 
+				new Date(1990,10,10), "MailTest@mailtest.test",
+				"5 rue des tests", "testpicture.hostTest.test",
+				true, true,
+				"m0td3p4ssTest"
+				);
+		assertEquals("test addPersonne(EntityPersonne)", "Personne added", sp.addPersonne(eP));
 	}
 
 	@Test
