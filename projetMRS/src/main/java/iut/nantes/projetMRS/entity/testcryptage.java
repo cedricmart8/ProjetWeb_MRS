b70a2dba508a93eecbf3a3bfc626b4a0d184c6bb
samixchoumi <samixchoumi@gmail.com>
2018-03-12 15:16:41 +0100

    ajouit test cryptage pour cedric

---
+++ projetMRS/src/main/java/iut/nantes/projetMRS/entity/testcryptage.java
@@ -0,0 +1,16 @@
+package iut.nantes.projetMRS.entity;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+
+public class testcryptage {
+
+	public static void main(String[] args) throws NoSuchAlgorithmException {
+		// TODO Auto-generated method stub
+		EntityPersonne eP = new EntityPersonne("Samix","Choumi",new Date(), "samixchoumi@mail.com", "adressequelconque",
+				null,true,true,"mdp415du4428FUTUR");
+		String mdpCrypte = eP.getMotDePasse();
+		System.out.println(eP.testDuMotDePasseCrypte("mdp415du4428FUTUR", mdpCrypte));
+	}
+
+}
