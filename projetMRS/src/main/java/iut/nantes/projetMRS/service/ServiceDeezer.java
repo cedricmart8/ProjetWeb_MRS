98611afddb6b79cd88bc3a7f5bc58c12e76d2350
samixchoumi <samixchoumi@gmail.com>
2018-03-13 14:07:12 +0100

    ajout des test de getDataGenre

--- projetMRS/src/main/java/iut/nantes/projetMRS/service/ServiceDeezer.java
+++ projetMRS/src/main/java/iut/nantes/projetMRS/service/ServiceDeezer.java
@@ -24,6 +24,22 @@ public class ServiceDeezer {
 	MongoClient client = new MongoClient("localhost", 8081); // connect to mongodb
 	Datastore datastore = new Morphia().createDatastore(client, "service"); // select service collection
 	
+	public MongoClient getClient() {
+		return client;
+	}
+
+	public void setClient(MongoClient client) {
+		this.client = client;
+	}
+
+	public Datastore getDatastore() {
+		return datastore;
+	}
+
+	public void setDatastore(Datastore datastore) {
+		this.datastore = datastore;
+	}
+
 	public boolean getDataDeezerGenre(){
 		System.out.println("Utilisation getDataDeezerGenre()");
 		try {
--- projetMRS/src/test/java/iut/nantes/projetMRS/service/ServiceDeezerTest.java
+++ projetMRS/src/test/java/iut/nantes/projetMRS/service/ServiceDeezerTest.java
@@ -6,11 +6,22 @@ import org.junit.Test;
 
 public class ServiceDeezerTest {
 
+	ServiceDeezer sd = new ServiceDeezer();
+	
 	@Test
 	public void testGetDataDeezerGenre() {
 		System.out.println("Test GetDataDeezerGenre\n\n\n");
 		
-		assertEquals("test getDataGenreDeezer", true, true);
+		assertEquals("test getDataGenreDeezer", true, sd.getDataDeezerGenre());
+	}
+	
+	@Test
+	public void testGetDataDeezerGenreError() {
+		System.out.println("Test GetDataDeezerGenreError\n\n\n");
+		
+		sd.setClient(null);
+		
+		assertEquals("test getDataGenreDeezer", false, sd.getDataDeezerGenre());
 	}
 
 }
